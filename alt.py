6db8adf99b88f2307fa6d269321b490d54a84991
mdutras <dutra22ms@gmail.com>
2022-11-03 15:07:25 -0300

    Adicionei stratified Kfold e uma nova métrica para análise dos classificadores, além disso criei um arquivo para testes

--- alt.py
+++ alt.py
@@ -1,18 +1,18 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
 import nltk
 import re
 import gensim
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, precision_score
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.naive_bayes import MultinomialNB
 
 def customTokenizer(doc):
     stopWords = list(set(nltk.corpus.stopwords.words('english')) | set(gensim.parsing.preprocessing.STOPWORDS))
     st = nltk.stem.PorterStemmer()
-    doc = doc.lower()
-    doc = re.sub(r"[,.;@#?!&$'\"0-9]+", " ", doc)
+    doc = re.sub(r"[,.;@#?!&$'\"0-9]+", " ", doc.lower())
     doc = nltk.word_tokenize(doc)
     doc = list(filter(lambda s : s not in stopWords, doc))
     doc = [st.stem(w) for w in doc]
@@ -20,26 +20,40 @@ def customTokenizer(doc):
 
 def featureVector(docs):
     # Term Frequency
-    countVec = CountVectorizer(min_df=1, tokenizer=customTokenizer)
+    print("Pré-processamento")
+    countVec = CountVectorizer(min_df=5, tokenizer=customTokenizer)
     TF = countVec.fit_transform(docs)
     # Inverse Document Frequency
     tfidfTransf = TfidfTransformer(smooth_idf=True,use_idf=True)
     tfidfTransf.fit(TF)
     IDF = countVec.transform(docs)
     # TF-IDF
+    print("TF-IDF")
     TFIDF = tfidfTransf.transform(IDF)
-    #print(countVec.get_feature_names_out())
-    #print(pd.DataFrame(TFIDF.todense().tolist(), columns=countVec.get_feature_names_out()))
-    #print(TFIDF.todense().tolist())
     return TFIDF.todense().tolist()
 
-def SVMclass(features, classes, kernel):
-    xTrain, xTest, yTrain, yTest = train_test_split(features, classes, train_size=0.7, test_size=0.3,random_state=109)
-    classifier = sklearn.svm.SVC(kernel=kernel).fit(xTrain, yTrain)
-    prediction = classifier.predict(xTest)
-    accuracy = accuracy_score(yTest, prediction)
-    f1 = f1_score(yTest, prediction, average='weighted')
-    print(f"--- {kernel}\nAccuracy = {accuracy}\nF1 = {f1}")
+def classifier(features, classes, classif, extra={}):
+    print("Classificador")
+    #docsTrain, docsTest, classesTrain, classesTest = train_test_split(features, classes, train_size=0.7, test_size=0.3,random_state=109)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=13)
+    measure = {
+        "precision": 0,
+        "accuracy": 0,
+        "recall": 0,
+        "f1": 0
+    }
+    for train, test in kf.split(features, classes):
+        model = classif.fit(features[train], classes[train])
+        prediction = np.array(model.predict(features[test]))
+        measure["accuracy"] += accuracy_score(classes[test], prediction)
+        measure["precision"] += precision_score(classes[test], prediction, average='weighted')
+        measure["recall"] += recall_score(classes[test], prediction, average='weighted')
+        measure["f1"] += f1_score(classes[test], prediction, average='weighted')
+        #print(f"--- {extra["classificador"]}\nAccuracy = {accuracy}\nPrecision = {precision}\nRecall = {recall}\nF1 = {f1}")
+    for key in measure:
+        measure[key] /= kf.get_n_splits()
+    print(measure)
+
 
 def main():
     # docs = ["I wish I loved the Human Race;",
@@ -50,14 +64,10 @@ def main():
     # "I wish I thought \"What Jolly Fun!\""]
     # classes = [0,1,2,1,0]
     data = pd.read_csv('data.csv')
-    classes = data['genre']
-    docs = data['synopsis']
-    features = featureVector(docs)
-    SVMclass(features, classes, "linear")
-    SVMclass(features, classes, "poly")
-    SVMclass(features, classes, "rbf")
-    SVMclass(features, classes, "sigmoid")
-    SVMclass(features, classes, "precomputed")
+    classes = np.array(data['genre'])
+    docs = np.array(data['synopsis'])
+    features = np.array(featureVector(docs))
+    classifier(features, classes, sklearn.svm.SVC(kernel='linear'))
 
 if __name__ == "__main__":
     main()
